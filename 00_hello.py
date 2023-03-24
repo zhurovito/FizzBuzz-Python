36b0b302bc47907d8137a75fb5ba916ffd1ea871
HelioGit <helizhurov@gmail.com>
2023-03-24 17:42:32 +0100

    version2

---
+++ 00_hello.py
@@ -0,0 +1,7 @@
+#hola mundo esto es un comentario
+print("Hola mundo")
+print("Hola mundo")
+print(type("P"))#tipo de dato string
+print(type(5))#tipo de dato int
+print(type(5+2))#tipo de dato complejo
+print(type(True))#tipo booleano
---
+++ fizzbuzz.py
@@ -0,0 +1,48 @@
+##Escribir un código en cualquier lenguaje de programación (incluso pseudocódigo) 
+# que represente en pantalla los números del 1 al 100, sustituyendo los múltiplos de 3 por la palabra “fizz”, 
+# los múltiplos de 5 por “buzz” 
+# y los múltiplos de ambos, es decir, los múltiplos de 3 y 5 (o de 15), por la palabra “fizzbuzz”.for i in range(1,101):
+    
+#fizz buzz#
+# 
+# 
+# # 
+
+for i in range(1,101):
+
+    if i % 3==0 and  i % 5==0:
+
+        print("FizzBuzz")
+
+    elif i % 3==0:
+
+        print("Fizz")
+    elif i % 5==0:
+        print("Buzz")
+    else:
+
+        print(i)    
+
+
+
+
+
+
+
+
+
+
+
+
+##numero par##
+
+# i:int
+
+# for i in range(1,50):
+
+#         if i %2 ==0:
+#            print ('El numero ' ,(i),' es par\n',end='')
+      
+#         else:
+#             print('El numero',(i),' es impar\n',end='')
+          
